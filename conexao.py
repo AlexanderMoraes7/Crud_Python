f81b83efd6f898b272e28588c98b950fbab65111
Alex <alexander97643@gmail.com>
2024-04-28 16:40:42 -0300

    deleted:    Main.py 	new file:   __pycache__/conexao.cpython-311.pyc 	new file:   conexao.py 	modified:   config.ini 	new file:   teste.py

---
+++ conexao.py
@@ -0,0 +1,38 @@
+"""Testing CRUD with Python"""
+import mysql.connector
+
+
+class Conexao:
+    """Class representing a Conection"""
+    __bancoconexao = mysql.connector.connect(
+        host='localhost',
+        database='dbteste',
+        user='root',
+        password='masterkey',
+    )
+    __cursor = __bancoconexao.cursor()
+
+    def inserir(self, nome_produto, valor):
+        """Method insert in database"""
+        comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+        self.__cursor.execute(comando)
+        self.__bancoconexao.commit()  # Edita o banco de dado
+        # resultado = __cursor.fetchall() # Ler o banco de dados
+
+    def deletarporid(self, idproduto):
+        """Method delete in database"""
+        comando = f'DELETE FROM vendas WHERE idvendas = {idproduto}'
+        self.__cursor.execute(comando)
+        self.__bancoconexao.commit()
+
+    def visualizar(self):
+        """Method visualization"""
+        comando = 'SELECT * FROM vendas'
+        self.__cursor.execute(comando)
+        resultado = self.__cursor.fetchall()
+        print(resultado)
+
+    def fecharconexao(self):
+        """Method to close the connection"""
+        self.__cursor.close()
+        self.__bancoconexao.close()
---
+++ teste.py
@@ -0,0 +1,14 @@
+"""Import the conection of database"""
+import conexao
+
+
+class Provisorio(conexao.Conexao):
+    """Testing class"""
+    pass
+
+
+novaconexao = Provisorio()
+novaconexao.inserir('ovo20unidade', 22.30)
+# novaconexao.deletarporid(9)
+novaconexao.visualizar()
+novaconexao.fecharconexao()
